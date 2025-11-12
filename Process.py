07276644ab8650dd87ff53a2edc9b164c13fcfaf
git-youmesh <umesh.bomme.gowda@gmail.com>
2025-11-12 14:50:26 +0530

    Implement PDF loading and similarity search

---
+++ Process.py
@@ -0,0 +1,47 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+loader = PyPDFLoader('Test.pdf')
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,  # Adjust as needed
+        chunk_overlap=200,  # Adjust as needed for context
+        separators=["\n\n", "\n", " ", ""],  # Default separators prioritizing paragraphs
+    )
+
+documents = loader.load()
+    # Or load page by page if you prefer
+    # documents = loader.load()
+
+#pages = loader.load_and_split()
+split_documents = text_splitter.split_documents(documents)
+
+texts = [doc.page_content for doc in split_documents]
+from langchain_chroma import Chroma
+import csv
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+    # Load a pre-trained Hugging Face embedding model
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+chroma_db = Chroma.from_texts(
+    texts=texts,
+    collection_name='db_docs',
+    collection_metadata={"hnsw:space": "cosine"},  # Set distance function to cosine
+embedding=embeddings
+)
+
+
+
+similarity_threshold_retriever = chroma_db.as_retriever(search_type="similarity_score_threshold",
+search_kwargs={"k": 3,"score_threshold": 0.3})
+
+query = "what steps to be implmented limit conflicts of interests ?"
+ 
+results = chroma_db.similarity_search(query=query, k=3)
+print("**********************************")
+print(query)
+for doc in results:
+      print(f"* {doc.page_content} [{doc.metadata}]")
+     
+
+
